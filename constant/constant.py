8cf27f02ee5e53efb683b0be37789106e4946e85
Choi-jinwoo <chlwlsdn0828@gmail.com>
2020-11-24 15:20:23 +0900

    Add: append more info

--- constant/constant.py
+++ constant/constant.py
@@ -1,5 +1,7 @@
 SERVICE_KEY="t%2FXgWfBlkFqZI5SM9%2FqnpiSo6iMT%2FmtCeb7Uk3ILQXpVpjCh1lVAZmJmtR3y6HwYphXsGPZZvsTie0PePNfjIQ%3D%3D"
-URL = "http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson"
+TOTAL_SERVICE_KEY="%2B8KVYXhkChcsF%2F6mdz3UFqA%2BgJu5FzbZh7XE%2FkmUS8t4qgqjZCf%2BAQiRgvohHYtPWbo4SkXv8DAhw4TjWe16nw%3D%3D"
+SIDO_URL = "http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson"
+TOTAL_URL = "http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson"
 
 CLIENT_ID = "jpGpYbqPAFp1Y_Kt2yo8"
 CLIENT_SECRET = "5kV7BZ72UK"
--- controller/corona_controller.py
+++ controller/corona_controller.py
@@ -6,41 +6,74 @@ import json
 import requests
 
 service_key = constant.SERVICE_KEY
-url = constant.URL
+total_service_key = constant.TOTAL_SERVICE_KEY
+sido_url = constant.SIDO_URL
+total_url = constant.TOTAL_URL
 
 
 def get_corona_status(region):
-    response = requests.get(f'{url}?ServiceKey={service_key}&numOfRows=1000')
 
-    parsed_data = xmltodict.parse(response.text)
-    res_dict = json.loads(json.dumps(parsed_data))
+    # 전국 단위 API 호출
 
-    with open('corona.pickle', 'wb') as fw:
-        pickle.dump(res_dict, fw)
+    total_response = requests.get(f'{total_url}?ServiceKey={total_service_key}&numOfRows=1000')
+    total_parsed_data = xmltodict.parse(total_response.text)
+    total_res_dict = json.loads(json.dumps(total_parsed_data))
+
+    # 전국 더미 만들기
+
+    with open('total_corona.pickle', 'wb') as fw:
+        pickle.dump(total_res_dict, fw)
+
+    total_corona = {}
+    total_data = {}
 
     try:
-        raise Exception('오류')
-        corona_list = res_dict['response']['body']['items']['item']
+        total_corona = total_res_dict['response']['body']['items']['item']
+    except:
+        with open('total_corona.pickle', 'rb') as fr:
+            total_corona = pickle.load(fr)['response']['body']['items']['item']
 
-        if region is None:
-            return corona_list[-1]
+    total_data = {
+        'resutlNegCnt': total_corona['resutlNegCnt'],
+        'careCnt': total_corona['careCnt'],
+        'accExamCnt': total_corona['accExamCnt'],
+    }
 
-        for corona_item in corona_list:
-            if corona_item['gubun'] == region:
-                return corona_item
+    # 시도 단위 API 호출
+
+    sido_response = requests.get(f'{sido_url}?ServiceKey={service_key}&numOfRows=1000')
+
+    sido_parsed_data = xmltodict.parse(sido_response.text)
+    sido_res_dict = json.loads(json.dumps(sido_parsed_data))
+
+    with open('corona.pickle', 'wb') as fw:
+        pickle.dump(sido_res_dict, fw)
+
+    region_data = {}
+    corona_list = []
+    try:
+        corona_list = sido_res_dict['response']['body']['items']['item']
     except:
         with open('corona.pickle', 'rb') as fr:
-            res_dict = pickle.load(fr)
-            corona_list = res_dict['response']['body']['items']['item']
+            corona_list = pickle.load['response']['body']['items']['item']
 
-            if region is None:
-                return corona_list[-1]
+    is_valid_region = False
 
-            for corona_item in corona_list:
-                if corona_item['gubun'] == region:
-                    return corona_item
+    if region is None:
+        region_data = corona_list[-1]
+    else:
+        for corona_item in corona_list:
+            if corona_item['gubun'] == region:
+                region_data = corona_item
+                is_valid_region = True
+                break
+        if not is_valid_region:
+            return None
 
-    return None
+    region_data.update(total_data)
+
+    return region_data
+
 
 def get_corona_info(date, region):
     response = requests.get(f'{url}?ServiceKey={service_key}&numOfRows=1000&startCreateDt={date}&endCreateDt={date}')
