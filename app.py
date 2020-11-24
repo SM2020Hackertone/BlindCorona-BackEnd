7ff32636f82ae5651eda8e49e6af9bdc1018b6db
Choi-jinwoo <chlwlsdn0828@gmail.com>
2020-11-24 14:12:41 +0900

    Add: allow cors

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
 from controller import corona_controller, news_controller
+from flask_cors import CORS
 import requests
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/corona-status')
