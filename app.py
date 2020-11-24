33d3241d5f5cc25ccd524e8580606340633a7225
Choi-jinwoo <chlwlsdn0828@gmail.com>
2020-11-24 20:48:53 +0900

    Fix: dummy

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ CORS(app)
 @app.route('/corona-status')
 def corona_status():
     region = request.args.get('region', None)
-    data = corona_controller.get_corona_status(region, "20201030")
+    data = corona_controller.get_corona_status(region, None)
 
     if data is None:
         return {'message': '검증 오류'}, 400
