227e5b69b1c4322abbf8920ba414a7dba65ab3c3
jjmin321 <jjmin321@naver.com>
2020-11-24 11:45:49 +0900

    [ADD] corona-info API

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from controller import corona_controller, news_controller
+import requests
 
 app = Flask(__name__)
 
@@ -14,11 +15,22 @@ def corona_status():
 
     return data
 
+@app.route('/corona-info')
+def corona_info():
+    args = request.args.get('text', None)
+    if args is None:
+        return {'message': '음성 메세지를 입력해주세요'}, 400
+    language_processing_server = "http://172.30.1.58:3000/analyze"
+    parameter = {'text': args}
+    r = requests.get(language_processing_server, parameter)
+    date = r.json()['Date']
+    region = r.json()['Location']
+    data = corona_controller.get_corona_info(date, region)
+    return {'data': data}, 200
 
 @app.route('/news')
 def corona_news():
     data = news_controller.get_news()
-
     return data
 
 
